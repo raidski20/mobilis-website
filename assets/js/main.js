a05b37879e5052c3b5807ec0fcdfc45affae79a4
raid21 <raidski20@gmail.com>
2022-04-30 18:28:03 +0100

    Added animation to navbar opening & closing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
-$(document).ready(function() {
-    $('#menu-toggler').click(function() {
-        $('#navbar-section-links').toggleClass('d-none');
-    });
+document.getElementById("menu-toggler").addEventListener("click", function(e) {
+    e.stopPropagation()
+    let navLinks = document.getElementById("navbar-section-links");
+    if(navLinks.classList.contains("d-none"))
+    {
+        navLinks.classList.remove("d-none");
+        setTimeout(function () {
+            navLinks.classList.remove("opacity-0");
+        }, 20);
+    }
+    else
+    {
+        navLinks.classList.add("opacity-0");
+        navLinks.addEventListener('transitionend', function(e) {
+            navLinks.classList.add("d-none");
+        },
+        {
+            once: true,
+        });
+    }
 });
